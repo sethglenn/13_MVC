67389d8f8e4cd8cdc99d69a5379606a57f65347d
Seth Glenn <Sbgees@yahoo.com>
2020-12-12 17:08:21 -0600

    implemented get and post methods inside controller

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -1,6 +1,25 @@
 const express = require("express");
-const router = express.Router();
+const app = express.Router();
 const burger = require("../models/burger");
 
+app.get("/", function (req, res) {
+    burger.selectAll(function(data){
+        const handlebarObject = {burgers: data};
+        res.render("index", handlebarObject);
+    })
+});
 
+app.post("/burger/create", function(req, res){
+    burger.insertOne(req.body.burger_name, function(){
+        res.redirect("/");
+    })
+});
+
+app.post("/burger/eat", function(req, res){
+    burger.updateOne(req.body.id, function(){
+        res.redirect("/");
+    })
+});
+
+module.exports = app;
 
