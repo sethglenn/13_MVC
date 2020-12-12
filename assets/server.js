098bd2698889610168f8374003940d756be8a9ff
Seth Glenn <Sbgees@yahoo.com>
2020-12-11 21:03:06 -0600

    created db file, connection, and orm

---
+++ assets/server.js
@@ -0,0 +1,3 @@
+const express = require("express");
+const  mySQL = require("mysql");
+const handleBars = require("express-handlebars");
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ("Baconator");
+INSERT INTO burgers (burger_name) VALUES ("PepperJackBurger");
+INSERT INTO burgers (burger_name) VALUES ("Vegetarian");
+INSERT INTO burgers (burger_name) VALUES ("DoubleCheese");
