5b80e189b0a641f21f4cd9094a5bf58336c7018f
Seth Glenn <Sbgees@yahoo.com>
2020-12-11 21:20:27 -0600

    added methods for orm.js file to interact with database

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,29 @@
-const connect = require("./connection");
+const connection = require("./connection");
+
+const orm = {
+    selectAll: function(tablename, cb){
+        connection.query("SELECT * FROM ??,", [tablename], function(err, res){
+            if (err) throw err;
+
+            cb(res);
+        })
+    },
+    insertOne: function (tablename, colname, burger_name, cb) {
+        connection.query("INSERT INTOP ?? (??) VALUES (?)", [tablename, colname, burger_name],
+        function (err, res){
+            if (err) throw err;
+            cb(res);
+        })
+    },
+    updateOne: function (tablename, colname, devoured, id, cb) {
+        connection.query("UPDATE ?? SET ?? = ? WHERE ID = ?", [tablename, colname, devoured, id],
+        function (err, res){
+            if (err) throw err;
+            return (res);
+        })
+    }
+}
+
+
+
+module.exports = orm;
