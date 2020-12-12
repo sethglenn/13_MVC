cae310cdd31180288c62acf8656d9e908620ef25
Seth Glenn <Sbgees@yahoo.com>
2020-12-11 21:30:50 -0600

    implemented code for calling orm functions, created controller file

---
+++ models/burger.js
@@ -0,0 +1,26 @@
+const orm = require("../config/orm");
+
+const burger = {
+    selectAll: function(cb){
+        orm.selectAll("burgers", function(res){
+            cb(res);
+        });
+    },
+    insertOne: function(burger_name, cb){
+        orm.insertOne("burgers","burger_name", burger_name, function(res){
+            cb(res);
+        });
+    },
+    updateOne: function(burger_id, cb){
+        orm.updateOne("burgers", "devoured", true, burger_id, function(res){
+            cb(res);
+        });
+    },
+    restoreOne: function(burger_id, cb){
+        orm.updateOne("burgers", "devoured", false, burger_id, function(res){
+            cb(res);
+        });
+    }
+};
+
+module.exports = burger;
