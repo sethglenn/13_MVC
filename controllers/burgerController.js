2d299ed0c59265c0f6eb47e973fab13b4974eff0
Seth Glenn <Sbgees@yahoo.com>
2020-12-14 17:26:40 -0600

    trying to resolve local host error

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -2,20 +2,20 @@ const express = require("express");
 const app = express.Router();
 const burger = require("../models/burger");
 
-app.get("/", function (req, res) {
+app.get("/", (_req, res) => {
     burger.selectAll(function(data){
         const handlebarObject = {burgers: data};
         res.render("index", handlebarObject);
     })
 });
 
-app.post("/burger/create", function(req, res){
+app.post("/burger/create", (req, res) => {
     burger.insertOne(req.body.burger_name, function(){
         res.redirect("/");
     })
 });
 
-app.post("/burger/eat", function(req, res){
+app.post("/burger/eat", (req, res) => {
     burger.updateOne(req.body.id, function(){
         res.redirect("/");
     })
