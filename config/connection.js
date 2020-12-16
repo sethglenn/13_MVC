a9e2912aa8995eef3a92ab4fe49e5bd5072975fe
Seth Glenn <Sbgees@yahoo.com>
2020-12-15 23:11:37 -0600

    added variable in connection file

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,6 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
 if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
@@ -23,4 +24,5 @@ connection.connect(function(err) {
 });
 
 // Export connection for our ORM to use.
+connection.connect();
 module.exports = connection;
