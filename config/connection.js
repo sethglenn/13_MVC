658f8dd2af631b6281fdc18aef64244d64b362cc
Seth Glenn <Sbgees@yahoo.com>
2020-12-16 00:30:18 -0600

    heroku

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,15 @@
 var mysql = require("mysql");
 var connection;
 
-if (process.env.JAWSDB_URL) {
+if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {  
   connection = mysql.createConnection({
     host: "localhost",
+    port: 3306,
     user: "root",
-    password: "hacktheplanet",
-    database: "todoagain_db"
+    password: "yi48537ar6g46vfa",
+    database: "c79rmfxltq6c5nvz"
 });
 }
 
