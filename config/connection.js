0e65b56aca82ef693fef1946901d0da4e27f78df
Seth Glenn <Sbgees@yahoo.com>
2020-12-16 00:18:11 -0600

    heroku connection issues

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,17 @@
 var mysql = require("mysql");
 var connection;
 
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {  
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
 });
-
+}
 
 // Make connection.
 connection.connect(function(err) {
@@ -22,5 +24,5 @@ connection.connect(function(err) {
 });
 
 // Export connection for our ORM to use.
-connection.connect();
+connection.connect()
 module.exports = connection;
