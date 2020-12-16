92e7e01090cebf9f35f72e999a40145a82715426
Seth Glenn <Sbgees@yahoo.com>
2020-12-16 00:21:33 -0600

    heroku issues

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,14 @@
 var mysql = require("mysql");
 var connection;
 
-if(process.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {  
   connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
     user: "root",
-    password: "password",
-    database: "burgers_db"
+    password: "hacktheplanet",
+    database: "todoagain_db"
 });
 }
 
