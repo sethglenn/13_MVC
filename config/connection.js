523c90d5ed620d5ec779ac62052534be940490d5
Seth Glenn <Sbgees@yahoo.com>
2020-12-16 00:36:52 -0600

    heroku

--- config/connection.js
+++ config/connection.js
@@ -6,11 +6,11 @@ if(process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {  
   connection = mysql.createConnection({
-    host: "bqmayq5x95g1sgr9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port: 3306,
-    user: "x5rwime2s0jpqgfv",
-    password: "yi48537ar6g46vfa",
-    database: "c79rmfxltq6c5nvz"
+    host: "localhost",
+    // port: 3306,
+    user: "root",
+    password: "hacktheplanet",
+    database: "todoagain_db"
 });
 }
 
