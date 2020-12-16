ea473949708efdca2aeaafbe38240a3fa062b213
Seth Glenn <Sbgees@yahoo.com>
2020-12-16 00:40:13 -0600

    heroku

--- config/connection.js
+++ config/connection.js
@@ -7,10 +7,10 @@ if(process.env.JAWSDB_URL) {
 } else {  
   connection = mysql.createConnection({
     host: "localhost",
-    // port: 3306,
+    port: 3306,
     user: "root",
-    password: "hacktheplanet",
-    database: "todoagain_db"
+    password: "password",
+    database: "burgers_db"
 });
 }
 
